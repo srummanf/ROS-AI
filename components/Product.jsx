651b6fc4708682f1f7aa04967f5ba95501672dc2
Rumman <rummanfardeen4567@gmail.com>
2023-09-10 21:14:45 +0530

    🎨 Files modified

--- components/Product.jsx
+++ components/Product.jsx
@@ -69,7 +69,7 @@ const Product = () => {
 
 
                         <div className="text-white my-5">
-                            <div id="aslSignsContainer" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4 my-6">
+                            <div id="aslSignsContainer" className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4 my-6">
                                 {word.split('').map((char, index) => (
                                     <img
                                         key={index}
--- components/Team.jsx
+++ components/Team.jsx
@@ -8,7 +8,7 @@ const Team = () => {
                     <div class="container flex flex-col items-center justify-center p-4 mx-auto space-y-8 sm:p-10">
                         <h1 class="text-4xl font-bold leadi text-center sm:text-5xl">Our team</h1>
                         <p class="max-w-2xl text-center dark:text-gray-400">We the team of developers and a small tribute each of our contributions. </p>
-                        <div class="flex flex-row flex-wrap-reverse justify-center">
+                        <div class="flex flex-row flex-wrap justify-center">
                             <div class="flex flex-col justify-center m-8 text-center">
                                 <img alt="" class="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="rumi.jpeg" />
                                     <p class="text-xl font-semibold leadi">Shaikh Rumman Fardeen</p>
